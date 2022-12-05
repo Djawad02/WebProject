bd48777b2c3e6ad6002fc5da03de48c8a5e53fb6
Djawad02 <111452124+Djawad02@users.noreply.github.com>
2022-12-05 23:12:19 +0500

    Update app.js

--- project/app.js
+++ project/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+const User = require('./model/userSchema')
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/Project')
@@ -12,6 +13,10 @@ mongoose
   .catch((err) => {
     console.error('Error connecting to mongo', err.reason)
   })
+
+app.use(express.json());
+
+app.use(require('./routes/users'));  //linking router files
 
 app.get('/', (req, res) => {
     res.send('Home');
@@ -26,5 +31,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
 
