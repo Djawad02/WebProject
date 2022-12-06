88f84da8546b0239c78c003632b43ec47a25857e
RumaisaIlyas <119864397+RumaisaIlyas@users.noreply.github.com>
2022-12-06 22:24:28 +0500

    Update Login.js

--- project/react-app/src/components/Login.js
+++ project/react-app/src/components/Login.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from "react";
 
-const Login = () => {
+function Login() {
   return (
     <div>
-    
-      <h5>Login Page</h5>
+      <h1 className="text-center mt-4">Log in</h1>
+
+      <div className="container">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="text-center">
+            <button type="submit" class="btn btn-primary  m-4">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
