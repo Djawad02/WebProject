804d4a05384c21f693b2769e5e8207ba9b1ca59f
Djawad02 <111452124+Djawad02@users.noreply.github.com>
2022-12-06 19:20:34 +0500

    Update About.js

--- project/react-app/src/components/About.js
+++ project/react-app/src/components/About.js
@@ -16,8 +16,17 @@ const About = () => {
                   <h5>Profile View</h5>
                   <h6>web developer</h6>
                   <p className='profile-rating mt-3 mb-5'>Rankings<span>1-10</span></p>
-              </div>
 
+                  <ul className="nav nav-tabs" role="tablist">
+                  <li className="nav-item">
+                    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                  </li>
+                  <li className="nav-item">
+                  <a className="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"  aria-selected="false">Timeline</a>
+                  </li>
+                </ul>
+
+              </div>
           </div>
         </form>
       </div>
