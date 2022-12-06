ebd5cc6cdda8e2cfa8c47d7af9bbade84aabda7d
Djawad02 <111452124+Djawad02@users.noreply.github.com>
2022-12-06 18:30:43 +0500

    Update About.js

--- project/react-app/src/components/About.js
+++ project/react-app/src/components/About.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import person from '../Images/person.png'
 
 const About = () => {
   return (
-    <div>
-      
-      <h5>About us Page</h5>
-    </div>
+    <>
+      <div className="container emp-profile">
+        <form method="">
+          <div className="row">
+              <div className="col-md-4">
+                  <img src={person} alt="not loaded" />
+              </div>
+
+              <div className="col-md-6">
+                  <div className="profile-head"></div>
+                  <h5>Profile View</h5>
+                  <h6>web developer</h6>
+                  <p className='profile-rating mt-3 mb-5'>Rankings<span>1-10</span></p>
+              </div>
+
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
