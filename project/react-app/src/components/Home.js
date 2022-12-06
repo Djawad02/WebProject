a56773ec2ec29cc12ebba4c8aa6b131034b554fb
Djawad02 <111452124+Djawad02@users.noreply.github.com>
2022-12-06 18:31:38 +0500

    Update Home.js

--- project/react-app/src/components/Home.js
+++ project/react-app/src/components/Home.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h4> WELCOME to VeteranMeet</h4>
-      <h5>A web-based solution</h5>
+    <>
+      <div className="home">
+      <div className="home-div h-100 d-flex flex-column justify-content-center align-items-center">
+        <div className="d-flex justify-content-center">
+          <p className="fs-3" style={{letterSpacing:"0.5em",color:"skyblue"}}><b>WELCOME</b></p>
+        </div>
+          <h1>We are VeteranMeet</h1>
+          <p>VeteranMeet is web-based solution to connect with veterans and engage them into community
+            services based on their interests.</p>
+      </div>
     </div>
+    </>
   )
 }
 
