9ad9a28733f98172f9aba653a7653267b4628ead
Djawad02 <111452124+Djawad02@users.noreply.github.com>
2022-12-06 18:32:20 +0500

    Update Navbar.js

--- project/react-app/src/components/Navbar.js
+++ project/react-app/src/components/Navbar.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <NavLink className="navbar-brand" to="#">Navbar</NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+   <nav className="navbar navbar-expand-lg navbar-light bg-info">
+  
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
@@ -17,7 +14,7 @@ const Navbar = () => {
         <NavLink className="nav-link" to="/">Home <span className="sr-only"></span></NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" to="/about">About</NavLink>
+        <NavLink className="nav-link" to="/about">Profile</NavLink>
       </li>
       <li className="nav-item">
         <NavLink className="nav-link" to="/contact">Contact</NavLink>
