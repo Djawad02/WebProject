b775318c6cc7e404d293c2db89a25919c3406666
RumaisaIlyas <119864397+RumaisaIlyas@users.noreply.github.com>
2022-12-06 19:44:29 +0500

    Update Signup.js

--- project/react-app/src/components/Signup.js
+++ project/react-app/src/components/Signup.js
@@ -1,11 +1,80 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import signpic from "../images/logo192.png";
 
 const Signup = () => {
   return (
-    <div>
-      <h5>Signup Page</h5>
-    </div>
+    <>
+    <section className = "signup" >
+            <div className="container mt-5">
+              <div className = "signup-content">
+                    <div className='signup-form'>
+                        <h2 className='form-title'>Sign up</h2>
+                        <form className='register-form' id='register-form'>
+
+                          <div className='form-group'>
+                              <label htmlFor='name'>
+                                <i class="zmdi zmdi-account material-icons-name"></i>
+                              </label>
+                              <input type="text" name="name" id ="name" autoComplete='off'
+                              placeholder="Your Name"/>
+                          </div>
+                          <div className='form-group'>
+                              <label htmlFor='email'>
+                                <i class="zmdi zmdi-email material-icons-name"></i>
+                              </label>
+                              <input type="email" name="email" id ="email" autoComplete='off'
+                              placeholder="Your Email"/>
+                          </div>
+                          <div className='form-group'>
+                              <label htmlFor='phone'>
+                                <i class="zmdi zmdi-phone-in-talk material-icons-name"></i>
+                              </label>
+                              <input type="number" name="phone" id="phone" autoComplete='off'
+                              placeholder="Your Phone Number"/>
+                          </div>
+                          <div className='form-group'>
+                              <label htmlFor='profession'>
+                                <i class="zmdi zmdi-slideshow material-icons-name"></i>
+                              </label>
+                              <input type="text" name="profession" id="profession" autoComplete='off'
+                              placeholder="Your Profession"/>
+                          </div>
+                          <div className='form-group'>
+                              <label htmlFor='password'>
+                                <i class="zmdi zmdi-lock material-icons-name"></i>
+                              </label>
+                              <input type="password" name="password" id="password" autoComplete='off'
+                              placeholder="Your Password"/>
+                          </div>
+                          <div className='form-group'>
+                              <label htmlFor='cpassword'>
+                                <i class="zmdi zmdi-lock material-icons-name"></i>
+                              </label>
+                              <input type="password" name="cpassword" id="cpassword" autoComplete='off'
+                              placeholder="Confirm Your Password"/>
+                          </div>
+
+                          <div className='form-group form-button'>
+                            <input type = "submit" name ="signup" id="signup" className='form-submit' value="register"/>
+                          </div>
+
+                        </form>
+                        </div>
+                        <div className='signup-image'>
+                          <figure>
+                            <img src={signpic} alt="registeration pic"/>
+                          </figure>
+                          <NavLink to="/login" className="signup-image-link">I am already Registered</NavLink>
+                        </div>
+
+                   
+              </div>
+            </div>
+    </section>
+    </>
   )
 }
 
 export default Signup
+
