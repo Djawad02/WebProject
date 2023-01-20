dbb7675d8b76d3ea26553851c8576c0caee8f2ea
MarriumJilani <119864494+MarriumJilani@users.noreply.github.com>
2023-01-20 22:31:58 +0500

    Add files via upload

---
+++ server/routes/comauth.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const ComUser = require("../models/ComUser");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/com", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new ComUser({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/comlogin", async (req, res) => {
+  try {
+    const user = await ComUser.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
